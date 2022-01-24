475b24808137a76cc9029348744dc971f6eb4c40
Dina Adel <cc201029@fhstp.ac.at>
2022-01-24 07:48:05 +0100

    Created new activity (Implemented switching from first activity to second activity).

--- app/src/main/java/com/example/wheezer/MainActivity.kt
+++ app/src/main/java/com/example/wheezer/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.wheezer
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val secondActBtn = findViewById<Button> (R.id.second_act_btn)
+        secondActBtn.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
