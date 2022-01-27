8e7fe40af55a07f363f61c0f2fabef328c53ab60
Dina Adel <cc201029@fhstp.ac.at>
2022-01-27 22:13:59 +0100

    main activity removed

--- app/src/main/java/com/example/wheezer/SecondActivity.kt
+++ app/src/main/java/com/example/wheezer/SecondActivity.kt
@@ -1,11 +1,14 @@
 package com.example.wheezer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
     }
 }
