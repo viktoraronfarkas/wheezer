f023266b37308b6f9eff1e820d5cceff284bde6b
Dina Adel <cc201029@fhstp.ac.at>
2022-03-13 14:26:03 +0100

    last changes before work (still navbar doesn't work). Also there is an invisible Image button in the which needs to be visible on click

--- app/src/main/java/com/example/wheezer/StartExercise.kt
+++ app/src/main/java/com/example/wheezer/StartExercise.kt
@@ -2,10 +2,18 @@ package com.example.wheezer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class StartExercise : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_exercise)
+
+        val buttonSave = findViewById<ImageButton>(R.id.saveButton)
+        buttonSave.setOnClickListener {
+            val intent = Intent(this, NewActivity::class.java)
+            startActivity(intent)
+
+
     }
 }
